69ec65b738b4eee1016ab7fc888c80d466267d8f
Dimanth <69086089+Dimanth@users.noreply.github.com>
2020-11-04 18:32:24 +0530

    Create intern_and_employee_record_system_operations.c

---
+++ MiniProject_C/3_Implementation/src/intern_and_employee_record_system_operations.c
@@ -0,0 +1,59 @@
+#include <intern_and_employee_record_system_operations.h>
+
+int EmployeeData(int operand1, int operand2)
+{
+    return operand1 + operand2;
+}
+
+int Addemployee(int operand1, int operand2)
+{
+    return operand1 - operand2;
+}
+
+/*int Viewallemployee(int operand1, int operand2)
+{
+    return operand1 * operand2;
+}*/
+
+int Modifyemployee(int operand1, int operand2)
+{
+    return operand1 * operand2;
+}
+
+int Deleteemployee(int operand1, int operand2)
+{
+    return operand1 * operand2;
+}
+
+int InternData(int operand1, int operand2)
+{
+    return operand1 * operand2;
+}
+
+int Addintern(int operand1, int operand2)
+{
+    return operand1 - operand2;
+}
+
+int Viewallinterns(int operand1, int operand2)
+{
+    return operand1 * operand2;
+}
+
+int Modifyintern(int operand1, int operand2)
+{
+    return operand1 * operand2;
+}
+
+int Deleteintern(int operand1, int operand2)
+{
+    return operand1 * operand2;
+}
+
+int Exit(int operand1, int operand2)
+{
+    if(0 == operand2)
+        return 0;
+    else
+        return operand1 / operand2;
+}
